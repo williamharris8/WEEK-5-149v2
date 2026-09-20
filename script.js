be39e13a5d733058454073ce7a43e2b644c0471f
williamharris8 <williamharris8@student.bpcc.edu>
2026-09-20 12:03:34 -0500

    PHASE 2 COMPLETE

---
+++ script.js
@@ -0,0 +1,9 @@
+const moonPoem = document.getElementById("poem-moon");
+
+moonPoem.style.backgroundColor = "lightgray";
+
+const westPoems = document.querySelectorAll("#wall-west .poem");
+
+console.log(moonPoem);
+console.log(westPoems);
+
