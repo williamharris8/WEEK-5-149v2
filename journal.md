74767cf572d43385acaa710e7fbd32236308baf3
williamharris8 <williamharris8@student.bpcc.edu>
2026-09-20 13:51:06 -0500

    PHASE 4 COMPLETE

--- journal.md
+++ journal.md
@@ -3,3 +3,5 @@ PHASE 1 - I used <div> for the two walls and <article> for each poem. Inside eac
 PHASE 2 - getElementById returned a single element and that was the one "poem-moon". querySelectorAll returned a NodeList of two. It is holding both west wall poems.
 
 PHASE 3 - I did not write a line removing the poem from west wall. Called appendChild on the east wall. Its one node that changed position in the tree. Since it can only have one parent it was detached and reattached.
+
+PHASE 4 - createElement makes an element bit it only exists in memory as a JS object without a parent.  Could still set its text, class, and color while its detached from the page. That became visible when appenChild is called.
--- script.js
+++ script.js
@@ -14,3 +14,28 @@ const eastWall = document.getElementById("wall-east");
 eastWall.appendChild(rainPoem);
 
 rainPoem.style.backgroundColor = "yellow";
+
+const addPoemButton = document.getElementById("add-poem-btn");
+
+addPoemButton.addEventListener("click", function () {
+  const titleInput = document.getElementById("poem-title");
+  const linesInput = document.getElementById("poem-lines");
+  const color = document.getElementById("poem-color").value;
+  const wallId = document.getElementById("poem-wall").value;
+
+  const newPoem = document.createElement("article");
+  const newTitle = document.createElement("h3");
+  const newText = document.createElement("p");
+
+  newTitle.textContent = titleInput.value;
+  newText.textContent = linesInput.value;
+  newPoem.className = "poem";
+  newPoem.style.backgroundColor = color;
+
+  newPoem.appendChild(newTitle);
+  newPoem.appendChild(newText);
+  document.getElementById(wallId).appendChild(newPoem);
+
+  titleInput.value = "";
+  linesInput.value = "";
+});
